ff70d683b4f52da9f9cd7600db0a919424c7f154
IdentificaPix <IdentificaPix@gmail.com>
2025-11-17 09:12:30 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,20 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// Arquivo totalmente compatível com TypeScript
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), ''); // pega todas as variáveis do Vercel
+  const env = loadEnv(mode, process.cwd(), '');
 
   return {
-    base: "./", // essencial para deploy SPA
     server: {
       port: 3000,
       host: '0.0.0.0',
     },
     plugins: [react()],
     define: {
-      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      // As variáveis DEVEM vir como import.meta.env.VITE_*
+      'process.env.API_KEY': JSON.stringify(env.VITE_GEMINI_API_KEY),
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.VITE_GEMINI_API_KEY),
     },
     resolve: {
       alias: {
