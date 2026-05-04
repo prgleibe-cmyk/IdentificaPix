dd0dd18c59c7292f8dbb3ab99a1f25971457aed6
IdentificaPix <IdentificaPix@gmail.com>
2026-05-04 01:09:32 -0300

    Update useCloudSync.ts

--- hooks/reconciliation/useCloudSync.ts
+++ hooks/reconciliation/useCloudSync.ts
@@ -277,7 +277,7 @@ export const useCloudSync = ({
                         const currentUpdatedAt = current?.updatedAt ? new Date(current.updatedAt).getTime() : 0;
                         const incomingUpdatedAt = r.updatedAt ? new Date(r.updatedAt).getTime() : 0;
 
-                        if (current && incomingUpdatedAt + 2000 < currentUpdatedAt) {
+                        if (current && incomingUpdatedAt < currentUpdatedAt) {
                             console.log('[BLOCK_REGRESSION:HYDRATE] Ignorando item antigo do banco:', r.transaction.id);
                             return;
                         }
@@ -422,7 +422,7 @@ export const useCloudSync = ({
                             const currentUpdatedAt = current?.updatedAt ? new Date(current.updatedAt).getTime() : 0;
                             const incomingUpdatedAt = updated_at ? new Date(updated_at).getTime() : 0;
 
-                            if (incomingUpdatedAt + 2000 < currentUpdatedAt) {
+                            if (incomingUpdatedAt < currentUpdatedAt) {
                                 console.log('[BLOCK_REGRESSION] Ignorando update antigo do banco', {
                                     id,
                                     incomingUpdatedAt,
